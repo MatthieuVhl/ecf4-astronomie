7863bba65ae814fa82484f8c461089d96a61e12d
Your Name <you@example.com>
2023-08-17 10:59:54 +0200

    first

---
+++ src/main/java/com/example/ecf4astronomie/Ecf4AstronomieApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecf4astronomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecf4AstronomieApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Ecf4AstronomieApplication.class, args);
+    }
+
+}

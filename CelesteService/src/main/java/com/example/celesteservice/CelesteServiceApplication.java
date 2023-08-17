2f373eee1ac92d08018059cd2d5c2801638f1179
Your Name <you@example.com>
2023-08-17 12:56:16 +0200

    ecf4

---
+++ CelesteService/src/main/java/com/example/celesteservice/CelesteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.celesteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelesteServiceApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CelesteServiceApplication.class, args);
+    }
+
+}
---
+++ CelesteService/src/test/java/com/example/celesteservice/CelesteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.celesteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelesteServiceApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}

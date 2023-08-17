ed293fc71c555eaeb7b3adf5de9fd717c37924fa
Your Name <you@example.com>
2023-08-17 14:27:34 +0200

    ecf4

---
+++ Authentification/src/main/java/com/example/authentification/entity/Utilisateur.java
@@ -0,0 +1,29 @@
+package com.example.authentification.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public Utilisateur(String username, String encodedPassword) {
+        this.username = username;
+        this.password = encodedPassword;
+    }
+
+}
---
+++ Authentification/src/main/java/com/example/authentification/repository/UtilisateurRepository.java
@@ -0,0 +1,12 @@
+package com.example.authentification.repository;
+
+import com.example.authentification.entity.Utilisateur;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
+    Optional<Utilisateur> findByUsername(String username);
+}
